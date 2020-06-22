df9bf6eac7fb6e8235f9d41a6c677a7b4f4f5a23
Tom Radford <tomradford596@gmail.com>
2020-06-21 23:34:07 -0400

    Added both paddles

---
+++ Pong Project/Assets/PlayerControls.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public float speed = 10.0f;
+    public float boundY = 2.25f;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    // Sets up the Rigidbody2D
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    // Moves the paddle in response to key clicks
+    void Update()
+    {
+        // Obtains the velocity based on key input
+        var vel = rb2d.velocity;
+        if (Input.GetKey(moveUp))
+        {
+            vel.y = speed;
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            vel.y = -speed;
+        }
+        else
+        {
+            vel.y = 0;
+        }
+        rb2d.velocity = vel;
+
+        // Updates the position of the paddle, limits paddle to screen
+        var pos = transform.position;
+        if (pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+    }
+}
