889200478981a0a10edf8714d05973ba9d2ee76b
Tom Radford <tomradford596@gmail.com>
2020-06-22 18:45:12 -0400

    Added the Ball
    Includes code to govern:
    - Reset the ball's position
    - Restart the game
    - Collision with players

---
+++ Pong Project/Assets/BallControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Starts the balls movement in a random direction (left or right)
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand<1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    // Stops the ball and resets its position to the center of the board
+    void ResetBall()
+    {
+        rb2d.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+
+    // Brings the game to the starting state for rematches
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("GoBall", 1);
+    }
+
+    // Adjusts speed of the ball and paddle in response to a collision
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            Vector2 vel;
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2) + (collision.collider.attachedRigidbody.velocity.y / 3);
+            rb2d.velocity = vel;
+        }
+    }
+}
